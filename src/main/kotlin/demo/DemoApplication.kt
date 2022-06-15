ee3db62ef88f87883fd7cff1cdfbe06629412bf1
anton <ant.arhipov@gmail.com>
2022-06-15 15:01:12 +0300

    MessageResource with GET mapping

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,6 +2,9 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
@@ -9,3 +12,10 @@ class DemoApplication
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+}
+
