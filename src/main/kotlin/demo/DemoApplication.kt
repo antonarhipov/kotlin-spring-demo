89ce62b29d34d0e327a1d576a856fd8476e18797
anton <ant.arhipov@gmail.com>
2022-06-16 19:49:41 +0300

    Database support

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,8 +2,13 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @SpringBootApplication
 class DemoApplication
@@ -13,13 +18,29 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
     @GetMapping
-    fun index() = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+
 }
 
 data class Message(val id: String?, val text: String)
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+
+    fun findMessages(): List<Message> = db.query("select * from messages") { rs, _ ->
+        Message(rs.getString("id"), rs.getString("text"))
+    }
+
+    fun save(message: Message){
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update("insert into messages values ( ?, ? )", id, message.text)
+    }
+
+}
