656b9459df7f5e8678b937ade76982104f38d214
anton <ant.arhipov@gmail.com>
2022-07-13 14:15:09 +0300

    1. MessageResource renamed to MessageController 2. Added various examples for working with collections

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -17,25 +17,76 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource(val service: MessageService) {
+class MessageController(val service: MessageService) {
     @GetMapping
     fun index(): List<Message> = service.findMessages()
 
     @GetMapping("/{id}")
     fun index(@PathVariable id: String): List<Message> = service.findMessageById(id)
 
+    @GetMapping("/firstAndLast")
+    fun firstAndLast(): List<Message> {
+        val messages = service.findMessages()
+        return listOf(messages.first(), messages.last())
+    }
+
+    @GetMapping("/firstMessageLongerThan10")
+    fun firstMessageLongerThan10(): Message {
+        val messages = service.findMessages()
+        return messages.first { it.text.length > 10 }
+    }
+
+    @GetMapping("/firstMessageLongerThan10OrNull")
+    fun firstMessageOrNull(): Message {
+        val messages = service.findMessages()
+        return messages.firstOrNull { it.text.length > 10 }
+            ?: Message(null, "Default message")
+    }
+
+    @GetMapping("/filterMessagesLongerThan10")
+    fun filterMessagesLongerThan10(): List<Message> {
+        val messages = service.findMessages()
+        return messages.filter { it.text.length > 10 }
+    }
+
+    @GetMapping("/sortByLastLetter")
+    fun sortByLastLetter(): List<Message> {
+        val messages = service.findMessages()
+        return messages.sortedBy { it.text.last() }
+    }
+
     @GetMapping("/groups")
     fun groups(): Map<String, List<Message>> {
         val messages = service.findMessages()
         val groups = listOf("hello", "bye")
 
-        val map = messages.groupByTo(mutableMapOf()) { message ->
+        val map = messages.groupBy { message ->
             groups.firstOrNull {
                 message.text.contains(it, ignoreCase = true)
             } ?: "other"
         }
 
         return map
+    }
+
+    @GetMapping("/transformMessagesToListOfStrings")
+    fun transformMessagesToListOfStrings(): List<String> {
+        val messages = service.findMessages()
+        return messages.map { "${it.id} ${it.text}" }
+    }
+
+    @GetMapping("/averageMessageLength")
+    fun averageMessageLength(): Double {
+        val messages = service.findMessages()
+        return messages.map { it.text.length }.average()
+    }
+
+    @GetMapping("findTheLongestMessage")
+    fun findTheLongestMessage(): Message {
+        val messages = service.findMessages()
+        return messages.reduce { first, second ->
+            if (first.text.length > second.text.length) first else second
+        }
     }
 
     @PostMapping
